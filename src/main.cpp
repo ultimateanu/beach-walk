402791b73341f857ca720acf27364f254307f96e
Anu Bandi <ultimateanu@gmail.com>
2026-03-31 22:15:57 -0700

    Adding stb image lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <stb_image.h>
 
 int main(int argc, char *argv[]) {
-  std::cout << "hello world" << std::endl;
+  if (argc != 2) {
+    std::cerr << "Error: expected single input image" << std::endl;
+    return 1;
+  }
+
+  int width, height, channels;
+  unsigned char *data = stbi_load(argv[1], &width, &height, &channels, 0);
+  if (!data) {
+    std::cerr << "Error: failed to load image" << std::endl;
+    return 1;
+  }
+  stbi_image_free(data);
+
+  std::cout << "height:" << height << " width:" << width << std::endl;
   return 0;
 }
